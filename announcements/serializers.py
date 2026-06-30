9df3635e62aa55a5e5b73282dcf5add0e875923d
Kiprotich <103497009+Tech-goodat@users.noreply.github.com>
2026-06-30 23:03:17 +0300

    Fix product_name source in AnnouncementsSerializers

--- announcements/serializers.py
+++ announcements/serializers.py
@@ -4,7 +4,7 @@ from .models import *
 
 class AnnouncementsSerializers(serializers.ModelSerializer):
     business_name=serializers.CharField(source='business.business_name', read_only=True)
-    product_name=serializers.CharField(source='products.product_name', read_only=True)
+    product_name=serializers.CharField(source='product.product_name', read_only=True)
     class Meta:
         model=AnnouncementModel
         fields=['id', 'title', 'message', 'announcement_type', 'business', 'business_name', 'product', 'product_name', 'created_at']
