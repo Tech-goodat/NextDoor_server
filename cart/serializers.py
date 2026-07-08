ea73ac57587fef1c09635125e11c9ed6776cdc90
Felix Kiprotich <felixkiprotich2000@gmail.com>
2026-07-08 15:59:13 +0300

    fixed postgres issues

--- cart/serializers.py
+++ cart/serializers.py
@@ -4,11 +4,11 @@ from .models import *
 class CartItemSerializer(serializers.ModelSerializer):
     product_name=serializers.CharField(source="product.product_name", read_only=True)
     price=serializers.CharField(source="product.price", read_only=True)
-    business_name=serializers.CharField(source="product.business_name", read_only=True)
+    business_name=serializers.CharField(source="product.business.business_name", read_only=True)
 
 
     class Meta:
-        model=CartModel
+        model=CartItemModel
         fields=["id", "product", "product_name", "business_name", "price", "quantity"]
 
 class CartSerializer(serializers.ModelSerializer):
